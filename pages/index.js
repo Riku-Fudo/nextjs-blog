64ce45d7896d0a0e819386c7cc3debec4dbf2196
Riku Fudo <riku.fudo@rakus-partners.co.jp>
2022-11-08 16:13:11 +0900

    事前レンダリングとデータ取得

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,24 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from '../lib/posts';
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home( { allPostsData } ) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <section className={utilStyles.headingMd}>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <p>ここに自己紹介を記述</p>
         <p>
           (これは Next.js のサンプルページです - これから{' '}
